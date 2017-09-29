541a5992e78335710f8c54f3b5c1bd29daf21145
Wiley <wangpeng2399@163.com>
2017-09-29 17:46:56 +0800

    no message

--- www/build/main.js
+++ www/build/main.js
@@ -170,7 +170,7 @@ var HomePage = (function () {
 }());
 HomePage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-home',template:/*ion-inline-start:"/Users/wiley/WorkSpace/GitHub/WileyPro/ion-hot-code/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>Home</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <h2>Welcome to Ionic!</h2>\n  <p>\n    This starter project comes with simple tabs-based layout for apps\n    that are going to primarily use a Tabbed UI.\n  </p>\n  <p>\n    Take a look at the <code>src/pages/</code> directory to add or change tabs,\n    update any existing page or create new pages.\n  </p>\n</ion-content>\n'/*ion-inline-end:"/Users/wiley/WorkSpace/GitHub/WileyPro/ion-hot-code/src/pages/home/home.html"*/
+        selector: 'page-home',template:/*ion-inline-start:"/Users/wiley/WorkSpace/GitHub/WileyPro/ion-hot-code/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>Home</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <h2>Welcome to Ionic!</h2>\n</ion-content>\n'/*ion-inline-end:"/Users/wiley/WorkSpace/GitHub/WileyPro/ion-hot-code/src/pages/home/home.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
 ], HomePage);
